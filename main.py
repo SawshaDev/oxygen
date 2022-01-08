683a4885c02d411a697a96ee352473146a7b5aa5
callimarieYT <85613286+callimarieYT@users.noreply.github.com>
2022-01-08 02:20:12 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+import discord
+from discord.ext import commands
+
+import os
+
+
+cogs_folder="./cogs"
+
+
+
+
+
+token = "OTEyOTY3MDU2NTU3NzU2NDI2.YZ3o1A.vKLHHRfbCc9GnOueJb3pe45kz64"
+
+
+
+prefixes = ['>', '-', ',']
+
+
+bot = commands.Bot(command_prefix=prefixes , intents=discord.Intents.all())
+bot.remove_command('help')
+
+
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(status=discord.Status.idle , activity=discord.Activity(type=discord.ActivityType.watching, name=f"{str(len(bot.guilds))} Servers!"))
+    print("Logged in as \n{0.user}".format(bot))
+    print("-------------------------------------------------")
+
+
+for filename in os.listdir('./cogs'):
+  if filename.endswith('.py'):
+    bot.load_extension(f'cogs.{filename[:-3]}')
+    
+  else:
+    print(f'Unable to load {filename[:-3]}')
+
+
+bot.run(token)  
+
